0c02a711ce0099249fc8f63ee068c622a24d1276
BuildTools <unconfigured@null.spigotmc.org>
2022-03-30 20:30:46 -0500

    First steps for LoginPage
    Co-authored-by: bradley ellis <bradleyaellis@users.noreply.github.com>

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { connect } from '../../server/routes/user.routes'
 
-function LoginPage() {
+function LoginPage(setUser) {
+    const usernameInput = useRef(null);
+    const passwordInput = useRef(null);
+    const navigate = useNavigate();
     return (
-        <div>LoginPage</div>
+        <h1>Login Page</h1>
     )
 }
 
-export default LoginPage
+
+
+const mapStateToProps = (state) => {
+    return {};
+}
+
+const mapDispatchToProps = {
+    setUser
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
