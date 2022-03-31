95b18083904a875683b46b1749e7ece3863efe86
BuildTools <unconfigured@null.spigotmc.org>
2022-03-30 20:36:29 -0500

    Login page done?

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -5,8 +5,42 @@ function LoginPage(setUser) {
     const usernameInput = useRef(null);
     const passwordInput = useRef(null);
     const navigate = useNavigate();
+
+    const handleLogin = useCallback(async () => {
+        const username = usernameInput.current.value;
+        const password = passwordInput.current.value;
+        if (
+            username.length < 2 ||
+            username.length > 16 ||
+            password.length < 2 ||
+            password.length > 16
+        ) {
+            return;
+        }
+        const json = await login(username, password);
+        console.log(json);
+        //! remove console log here
+        if (!json.success) {
+            console.log(json.error);
+        } else {
+            setUser(json.data);
+            navigate("/search")
+        }
+    }, [])
     return (
-        <h1>Login Page</h1>
+        <div>
+            <h1>Login Page</h1>
+            <div>
+                <label htmlFor='username'>Enter Username: </label>
+                <input ref={usernameInput} type="text" id="username" />
+            </div>
+            <div>
+                <label htmlFor='password'>Enter Password: </label>
+                <input ref={passwordInput} type="text" id="password" />
+            </div>
+            <button onClick={handleLogin} />
+        </div>
+
     )
 }
 
