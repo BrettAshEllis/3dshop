ff8357d29a736a1660609458ff9a1c7cc58dd75c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-30 20:22:03 -0500

    Created Axios file(?), getting help from brother
    Co-authored-by: bradley ellis <bradleyaellis@users.noreply.github.com>

--- src/redux/reducers/search.reducers.js
+++ src/redux/reducers/search.reducers.js
@@ -13,3 +13,35 @@ export default function searchReducer(state = INITIAL_STATE, action) {
             return state;
     }
 }
+
+
+// export const fetchChannelRequest = () => {
+//     return { type: FETCH_CHANNEL_REQUEST }
+// };
+
+// export const fetchChannelSuccess = (channel) => {
+//     return { type: FETCH_CHANNEL_SUCCESS, channel }
+// };
+
+// export const fetchChannelFailure = (error) => {
+//     return { type: FETCH_CHANNEL_FAILURE, error }
+// };
+
+// export const fetchChannel = () => {
+//     return (dispatch) => {
+//         dispatch(fetchChannelRequest())
+//         axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=${parentSearch}`)
+//             .then(response => {
+//                 const channel = response.data.map((val) => ({
+//                     channel_title: val.items.snippet.title,
+//                     channel_id: val.items.id,
+//                     channel_desc: val.items.snippet.description,
+//                 }));
+//                 dispatch(fetchChannelSuccess(channel))
+//             })
+//             .catch(error => {
+//                 const errorMsg = error.message
+//                 dispatch(fetchChannelFailure(errorMsg))
+//             })
+//     }
+// }
